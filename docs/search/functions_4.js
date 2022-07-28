9c4bde20fe6131732e39663c280df73d5b32bf7f
Dominik Schlösser <dsc@dosc.net>
2022-07-28 12:58:04 +0200

    API simplifications

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sensordisplay_0',['SensorDisplay',['../classustd_1_1SensorDisplay.html#a361dbc7f6b6498ed30d1af3598573b1b',1,'ustd::SensorDisplay']]],
+  ['sensordisplay_0',['SensorDisplay',['../classustd_1_1SensorDisplay.html#aa984f44ab5dd08b9dc3bb734456a81c0',1,'ustd::SensorDisplay']]],
   ['setreferencealtitude_1',['setReferenceAltitude',['../classustd_1_1PressTempBMP180.html#ab28f303967524daa77e713f96474e783',1,'ustd::PressTempBMP180']]],
   ['startrelativealtitude_2',['startRelativeAltitude',['../classustd_1_1PressTempBMP180.html#a5a82c509f6eed4f945e85ce082bb0366',1,'ustd::PressTempBMP180']]]
 ];
