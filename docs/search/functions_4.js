3539cbfc781d90bf3e0636af7885b8cf43117f9d
Dominik Schlösser <dsc@dosc.net>
2022-08-28 15:31:09 +0200

    New integration- and gain options for tsl

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['illuminanceldr_0',['IlluminanceLdr',['../classustd_1_1IlluminanceLdr.html#ac8382d3eae8c36fc22e080b88ae876be',1,'ustd::IlluminanceLdr']]],
-  ['illuminancetsl2561_1',['IlluminanceTSL2561',['../classustd_1_1IlluminanceTSL2561.html#a1c83182b64cbb8bd86c184fee2388c62',1,'ustd::IlluminanceTSL2561']]]
+  ['illuminancetsl2561_1',['IlluminanceTSL2561',['../classustd_1_1IlluminanceTSL2561.html#ac7b3ddebba85e0d540e8182dd210485c',1,'ustd::IlluminanceTSL2561']]]
 ];
